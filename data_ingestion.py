1bf8e11d419efb4fefa7b6bd2292c55f14df52e1
drupad.khublani <drupad.khublani@keap.com>
2020-05-17 13:55:52 -0700

    add solar_pv

--- data_ingestion.py
+++ data_ingestion.py
@@ -8,67 +8,135 @@ import json
 import requests
 import pandas as pd
 import re
-
+import numpy as np
+from geopy.geocoders import Nominatim
 
 class DataIngestor:
 
-	def __init__(self, filename, emission_api):
+	def __init__(self, filename, emission_api,solar_api):
 		self.filename = filename
 		self.emission_api = emission_api
+		self.solar_api = solar_api
 
 	def get_emission_data(self):
 		"""Make a get request to get the emission data for the cities of United States
 		Parameters: emission_api - url/api to download emissions citywide data
 		Returns:
-		A dataframe for the emission data
+		A dataFrame for the emission data
 		"""
 		response = requests.get(self.emission_api)
 		# Print the status code of the response.
-		#print(response.status_code)
+		print(response.status_code)
 
 		emission_data = json.dumps(response.json())
 
 		df_emission_data = pd.read_json(emission_data)
-		def Clean_names(City_name):
-			"""To remove the state name from city column
-			using regex. everything after , in city column is removed
-			"""
-			if re.search(r'\,.*', City_name): 
-				pos = re.search(r'\,.*', City_name).start() 
-				return City_name[:pos] 
-			else:
-				return City_name
-		df_emission_data['city'] = df_emission_data['city'].apply(Clean_names) 
-		print(df_emission_data.head())
 		return df_emission_data
 
+	def get_solar_pv(self , number_of_months_of_year =  12):
+		"""Make a get request to get the solar_pv for the states of United States
+		Parameters: number_of_states = each state would be a row which we get from get_emission_data()
+		number_of_months_of_year
+		Returns:
+		A dataframe for the emission data
+		"""
+		state_list = ["Alabama","Alaska", "Arizona", "Arkansas", "California", "Colorado",
+					  "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+					  "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+					  "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+					  "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+					  "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+					  "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+					  "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+		number_of_states = len(state_list)
+		a = np.zeros(shape=(number_of_states, (number_of_months_of_year+1)), dtype=object)
+		for i in range(number_of_states):
+			solar_pv = []
+			if i == 1 :
+				solar_pv.append(state_list[i])
+				data = ['0']
+				for j in range(number_of_months_of_year):
+					solar_pv.append(data[0])
+			else:
+				response = requests.get(solar_api+state_list[i])
+				data = json.loads(json.dumps(response.json()))
+				data = pd.io.json.json_normalize(data['outputs'])
+				data = data['solrad_monthly']
+				solar_pv.append(state_list[i])
+				for j in range(number_of_months_of_year):
+					solar_pv.append(data[0][j])
+			a[i] = solar_pv
+		df_solar_pv = pd.DataFrame(a)
+		df_solar_pv = df_solar_pv.rename(columns={0: 'state'})
+		#Print the status code of the response.
+		print(response.status_code)
+		return df_solar_pv
+
+	def get_state_name(self):
+		state_list = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
+					  "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+					  "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+					  "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+					  "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+					  "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+					  "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+					  "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+		geolocator = Nominatim(user_agent="solar_app")
+
+		df = self.get_emission_data()
+		# Extracting coordinate from geocoded_column & reversing list to get the correct order of lat and long
+		df['coordinates'] = df['geocoded_column'].apply(lambda x: x['coordinates'][::-1])
+		df['location'] = df['coordinates'].apply(lambda x: geolocator.reverse(x))
+		df['address'] = df['location'].apply(lambda x: x.address)
+		# Dropping rows with invalid lat, long values
+		df = df.dropna(subset=['address'])
+		df['addr_list'] = df['address'].apply(lambda x: str.split(x, ','))
+		df['addr_list'] = df['addr_list'].apply(lambda x: [i.lstrip() for i in x])
+		df['state'] = df['addr_list'].apply(lambda x: [i for i in x if i in state_list])
+		df['state'] = df['state'].apply(lambda x: ", ".join(x))
+		# Dropping rows with invalid lat, long values
+		df = df.dropna(subset=['state'])
+		print(df[['coordinates', 'location', 'address', 'addr_list', 'state']])
+		return df
+
+	def get_city_name(column_name):
+		"""Removes the state name from city column
+		Parameters: column_name - name of the column in which cleaning is required
+		Returns: cleaned column
+		"""
+		if re.search(r'\,.*', column_name):
+			pos = re.search(r'\,.*', column_name).start()
+			city = column_name[:pos]
+			return city
+		else:
+			return column_name
 
 	def get_electricity_consumption(self):
 		"""Reads excel file for the electricity consumption
 		Parameters: filename - name of the excel file
 		Returns:
-		A dataframe for the emission data
+		A dataFrame for the emission data
 		"""
-		full_path = 'data/'+self.filename
+		full_path = 'data/' + self.filename
 		df_elec = pd.read_excel(full_path, skiprows=3)
-		#print(df_elec.head())
+		print(df_elec.head())
 		return df_elec
 
 	def final_data(self):
 		"""Combines the electricity consumption data & emission data
-		Parameters: filename - name of the excel file
-					emission_api - url/api to download emissions citywide data
 		Returns:
-		Combined dataframe containing citywide data for emission and power consumption
+		Combined dataFrame containing citywide data for emission and power consumption
 		"""
-		df_emission_data = self.get_emission_data()
+		df_emission_data = self.get_state_name()
 		df_elec = self.get_electricity_consumption()
-		df_final = pd.merge(df_emission_data, df_elec, left_on='city', right_on='Census Division\nand State')
-		print(df_final)
+		df_solar_pv = self.get_solar_pv()
+		df = pd.merge(df_emission_data, df_elec, left_on='state', right_on='Census Division\nand State')
+		df_final = pd.merge(df, df_solar_pv, left_on='state', right_on='state')
+		print(df_final.head())
 		return df_final
 
 if __name__ == '__main__':
+	solar_api = 'https://developer.nrel.gov/api/pvwatts/v6.json?api_key=cYPf9OMDLPB2r05Cy3lc3N5rRFAYoCgzkf2fwetp&system_capacity=4&azimuth=180&tilt=20&array_type=1&module_type=0&losses=14.08&address='
 	di = DataIngestor('elec_cost.xlsx', "https://data.cdp.net/resource/wii4-buw5.json?country=United States of America")
-	df1 = di.get_emission_data()
-	df2 = di.get_electricity_consumption()
-	df3=di.final_data()
+	df = di.final_data()
